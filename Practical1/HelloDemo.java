0f304a715ee94da8b01c18a34af05198e65efcaa
Karu6202023 <161305608+Karu6202023@users.noreply.github.com>
2024-03-07 01:59:14 +0800

    Delete Practical1 directory

--- Practical1/HelloDemo.java
+++
@@ -1,9 +0,0 @@
-public class HelloDemo{
-    public static void main (String[]atgs){
-        
-        HelloFrame frame = new HelloFrame();
-        frame.setVisible(true);
-        
-        
-    }
-}
